0b362d918e392b136fec8254707118d0a7d249ed
Tut1tam <dolzhich.maxim@yandex.ru>
2023-05-16 19:54:04 +0300

    UPD: register proto models

--- backend/film_service/admin.py
+++ backend/film_service/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Actor, Category, Film, Genre, UserFilms
+
+admin.site.register(Actor)
+admin.site.register(Category)
+admin.site.register(Film)
+admin.site.register(Genre)
+admin.site.register(UserFilms)
