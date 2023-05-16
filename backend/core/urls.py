84fa3c6a479898e9f7c650341e1c39c77dab9dc5
Tut1tam <dolzhich.maxim@yandex.ru>
2023-05-16 18:01:25 +0300

    UPD structure

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from film_service import urls as film_service_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", film_service_urls),
+
 ]
---
+++ backend/film_service/urls.py
@@ -0,0 +1,2 @@
+# import swagger_client
+
