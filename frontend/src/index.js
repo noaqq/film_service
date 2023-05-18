1189d20ee8535b6a8bf1808a9ee3d30685dd41c4
noaqq <maximbaymetov2002@icloud.com>
2023-05-18 20:16:34 +0300

    UPD

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Navigate, Route } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Route exact path="/">
+      <Navigate to="/RegisterAuth" />
+    </Route>
   </React.StrictMode>
 );
 
