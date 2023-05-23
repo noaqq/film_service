34bae1e36273c4555544dd94af35ec1bcebbb462
noaqq <maximbaymetov2002@icloud.com>
2023-05-23 21:36:19 +0300

    UPD: add Register component

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Register.module.css';
+
+function Register() {
+  return (
+    <div className={styles.item}>
+        
+    </div>
+  );
+}
+
+export default Register;
