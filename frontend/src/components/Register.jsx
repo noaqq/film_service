49c6ad8789f8d725be8d14ebc9c62a600019a348
noaqq <maximbaymetov2002@icloud.com>
2023-05-24 13:37:47 +0300

    UPD: update Register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Register.module.css';
+import './assets/styles/global.css';
 
 function Register() {
   return (
-    <div className={styles.item}>
-        
+    <div className='container mx-auto px-4'>
+      <div className={styles.item}>
+          
+      </div>
     </div>
   );
 }
