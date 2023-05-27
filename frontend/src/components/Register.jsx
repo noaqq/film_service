25efc09587d22c5b683b96729fae7fbb2c7508ca
noaqq <maximbaymetov2002@icloud.com>
2023-05-28 00:00:00 +0300

    UPD: update Register comp

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -3,83 +3,22 @@ import '../assets/styles/style.css';
 
 function Register() {
   return (
-    <div className="flex">
-        <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
-              <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900">Войдите или зарегистрируйтесь</h2>
-              <h2 className="mt-0 text-1xl font-bold tracking-tight text-gray-900 opacity-75">Чтобы пользоваться сервисом</h2>
-            </div>
-
-            <div className="mt-8">
-              <div className="mt-6">
-                <form action="#" method="POST" className="space-y-6">
-                  <div>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                      Email address
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="space-y-1">
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                      Password
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                        Remember me
-                      </label>
-                    </div>
-
-                    <div className="text-sm">
-                      <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                        Forgot your password?
-                      </a>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
+    <body className={styles.cont}>
+      <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <div class="absolute top-4 left-4 rounded-full bg-yellow-500 w-8 h-8 flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-white">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+          </svg>
         </div>
+        <h1 class="text-3xl font-bold text-yellow-500 mt-8 mb-4">Войдите или зарегистрируйтесь</h1>
+        <p class="text-lg text-gray-500 mb-8">Чтобы пользоваться сервисом</p>
+        <div class="relative w-full max-w-xs">
+            <input type="text" placeholder="Email или номер телефона" class="w-full px-4 py-2 border border-gray-400 rounded-md text-gray-700 focus:outline-none focus:border-yellow-500"/>
+            <span class="absolute top-2 left-4 text-gray-400 pointer-events-none">Email или номер телефона</span>
+        </div>
+         <button class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-md mt-4">Вход</button>
       </div>
+    </body>
   );
 }
 
