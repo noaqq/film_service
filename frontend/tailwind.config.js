ce5d57c7a7253e1c6ca860c069e9bbc50749efdd
noaqq <maximbaymetov2002@icloud.com>
2023-05-29 19:24:35 +0300

    UPD: update config color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-bg":
+          "background-image: linear-gradient(to bottom, rgb(31, 31, 31),rgb(13, 13, 13)",
+      },
+    },
   },
   plugins: [],
 };
